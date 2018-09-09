412541c1892cc54a21d0ec37fd05d3d2ccf65d54
KaWa <Karollo47@gmail.com>
2018-09-09 10:34:21 +0200

    first commit

---
+++ src/main/java/com/sda/chatappserver/wwkwkdmg/WwkwkdmgApplication.java
@@ -0,0 +1,12 @@
+package com.sda.chatappserver.wwkwkdmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwkwkdmgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwkwkdmgApplication.class, args);
+    }
+}
