06de3a66b7267cc558880a2ac2ebd5e0dbe3a82e
Łukasz Sierakowski <ls@gmail.com>
2018-09-22 15:16:44 +0200

    Fixed test using in memory database

--- src/test/java/com/sda/chatappserver/wwkwkdmg/WwkwkdmgApplicationTests.java
+++ src/test/java/com/sda/chatappserver/wwkwkdmg/WwkwkdmgApplicationTests.java
@@ -3,10 +3,13 @@ package com.sda.chatappserver.wwkwkdmg;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+@ComponentScan(basePackages = {"com.sda.chatappserver.wwkwkdmg.repository"})
+//@SpringBootTest
 public class WwkwkdmgApplicationTests {
 
     @Test
--- src/test/java/com/sda/chatappserver/wwkwkdmg/repository/UserRepositoryTest.java
+++ src/test/java/com/sda/chatappserver/wwkwkdmg/repository/UserRepositoryTest.java
@@ -1,52 +1,42 @@
 package com.sda.chatappserver.wwkwkdmg.repository;
 
 
-import com.sda.chatappserver.wwkwkdmg.WwkwkdmgApplicationTests;
 import com.sda.chatappserver.wwkwkdmg.model.User;
+import com.sda.chatappserver.wwkwkdmg.model.UserStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
-import java.util.List;
-
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 
-//@RunWith(SpringJUnit4ClassRunner.class)
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { WwkwkdmgApplicationTests.class })
-//@Transactional
-//@DirtiesContext
-public class UserRepositoryTest  {
+@DataJpaTest
+public class UserRepositoryTest {
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    @Autowired
-    DataSource dataSource;
+	@Test
+	public void should_Find_User_By_Nick_And_Password() {
+		// Arrange
+		String nick = "Bobek";
+		String password = "12345";
+		userRepository.save(new User(null, nick, password, UserStatus.away, "", "", false));
 
+		// Act
+		User result = userRepository.findUserByNickAndPassword(nick, password);
 
-    @Test
-    public void should_Find_User_By_Nick_And_Password(){
-        User result = userRepository.findUserByNickAndPassword("Bobek", "123");
-       // result.getNick();
-       // result.getPassword();
-        Integer userID = result.getId().intValue();
-        //String result2 = result1.toString();
-
-        assertThat(userID, is(10));
-    }
-
-
-
-
-        }
+		// Assert
+		assertThat(result.getId(), is(1l));
+		assertThat(result.getNick(), is(nick));
+		assertThat(result.getPassword(), is(password));
+	}
+}
 
