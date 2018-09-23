84b0b3fae26929023f5f284f3be773f7f5a190a5
KaWa <Karollo47@gmail.com>
2018-09-23 11:15:46 +0200

    added messages auto refresh.

--- src/main/java/com/sda/chatappserver/wwkwkdmg/controllers/ChatController.java
+++ src/main/java/com/sda/chatappserver/wwkwkdmg/controllers/ChatController.java
@@ -1,16 +1,11 @@
 package com.sda.chatappserver.wwkwkdmg.controllers;
 
-import com.sda.chatappserver.wwkwkdmg.model.FixedMessage;
 import com.sda.chatappserver.wwkwkdmg.services.FixedMessageService;
 import com.sda.chatappserver.wwkwkdmg.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class ChatController {
@@ -26,12 +21,6 @@ public class ChatController {
 
     @RequestMapping("/chatApp")
     public String displayMessages(Model model) {
-        List<FixedMessage> fixedMessageList = fixedMessageService.getFixedMessages();
-        Collections.reverse(fixedMessageList);
-        List<FixedMessage> limitedMessageList = fixedMessageList.stream().limit(25).collect(Collectors.toList());
-        Collections.reverse(limitedMessageList);
-
-        model.addAttribute("messages", limitedMessageList);
         model.addAttribute("users", userService.getUserListFromDb());
         return "chatApp";
     }
